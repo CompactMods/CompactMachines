9a645da39d84f3e7054529afe9da5c2851c6c893
thraaawn <david.hilberath@gmx.de>
2014-09-21 17:33:34 +0200

    Prevent players from placing machine walls
    If you still managed to place one or have placed one before
    this commit you can use an "atomic shrinking module" in creative
    mode to break the block. Sigh.
    
    Fixes #9

--- src/main/java/org/dave/CompactMachines/block/BlockInnerWall.java
+++ src/main/java/org/dave/CompactMachines/block/BlockInnerWall.java
@@ -1,8 +1,12 @@
 package org.dave.CompactMachines.block;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+import org.dave.CompactMachines.item.ItemAtomShrinker;
 import org.dave.CompactMachines.reference.Names;
 
 public class BlockInnerWall extends BlockCM {
@@ -17,6 +21,19 @@ public class BlockInnerWall extends BlockCM {
 
 	@Override
 	public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z) {
+		if (!world.isRemote && player instanceof EntityPlayerMP && player.capabilities.isCreativeMode) {
+			ItemStack playerStack = player.getCurrentEquippedItem();
+			if(playerStack != null && playerStack.getItem() instanceof ItemAtomShrinker) {
+				return super.removedByPlayer(world, player, x, y, z);
+			}
+		}
+
 		return false;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack) {
+		world.setBlockToAir(x, y, z);
+		return;
 	}
 }
