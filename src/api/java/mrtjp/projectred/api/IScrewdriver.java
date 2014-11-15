0f25ded462a381a1a4fb143dd5d719dbb2023afb
thraaawn <david.hilberath@gmx.de>
2014-11-15 10:26:54 +0100

    Added APIs to allow building
    Fixes #29

---
+++ src/api/java/mrtjp/projectred/api/IScrewdriver.java
@@ -0,0 +1,9 @@
+package mrtjp.projectred.api;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.player.EntityPlayer;
+
+public interface IScrewdriver
+{
+    public void damageScrewdriver(World world, EntityPlayer player); // Damage the item on usage
+}
