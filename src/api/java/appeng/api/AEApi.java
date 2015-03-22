0489664c5bcdd504f53644ce598b33c03b38e60a
thraaawn <david.hilberath@gmx.de>
2015-03-22 12:27:41 +0100

    Updated AE2 API to rv2.beta build 14

--- src/api/java/appeng/api/AEApi.java
+++ src/api/java/appeng/api/AEApi.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2013 AlgorithmX2
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -24,11 +24,11 @@
 package appeng.api;
 
 /**
- * 
+ *
  * Entry point for api.
- * 
+ *
  * Available IMCs:
- * 
+ *
  */
 public class AEApi
 {
@@ -37,8 +37,8 @@ public class AEApi
 
 	/**
 	 * API Entry Point.
-	 * 
-	 * @return the {@link IAppEngApi} or null if the instance could not be retrieved
+	 *
+	 * @return the {@link IAppEngApi} or null if the INSTANCE could not be retrieved
 	 */
 	public static IAppEngApi instance()
 	{
@@ -47,7 +47,7 @@ public class AEApi
 			try
 			{
 				Class c = Class.forName( "appeng.core.Api" );
-				api = (IAppEngApi) c.getField( "instance" ).get( c );
+				api = (IAppEngApi) c.getField( "INSTANCE" ).get( c );
 			}
 			catch (Throwable e)
 			{
