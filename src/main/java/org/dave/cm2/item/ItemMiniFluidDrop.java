5fb8989cb23b3c2f27c6ca136719bcd7fd4396e9
thraaawn <david.hilberath@gmx.de>
2017-03-10 15:19:33 +0100

    Fluid Drop right click on IFluidHandler fills the handler now

--- src/main/java/org/dave/cm2/item/ItemMiniFluidDrop.java
+++ src/main/java/org/dave/cm2/item/ItemMiniFluidDrop.java
@@ -1,13 +1,22 @@
 package org.dave.cm2.item;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import org.dave.cm2.CompactMachines2;
+import org.dave.cm2.init.Fluidss;
 import org.dave.cm2.init.Potionss;
 import org.dave.cm2.misc.ConfigurationHandler;
 import org.dave.cm2.misc.CreativeTabCM2;
@@ -16,7 +25,6 @@ public class ItemMiniFluidDrop extends ItemFood {
     public ItemMiniFluidDrop() {
         super(1, 0.1F, false);
 
-        //this.setPotionEffect(new PotionEffect(Potionss.miniaturizationPotion, 400, 1, false, false), 0.8f);
         this.setCreativeTab(CreativeTabCM2.CM2_TAB);
     }
 
@@ -26,6 +34,30 @@ public class ItemMiniFluidDrop extends ItemFood {
             name = CompactMachines2.MODID + "." + name;
         }
         return super.setUnlocalizedName(name);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack itemStack, World world, EntityPlayer player, EnumHand hand) {
+        if(world.isRemote || !(player instanceof EntityPlayerMP)) {
+            return super.onItemRightClick(itemStack, world, player, hand);
+        }
+
+        RayTraceResult trace = player.rayTrace(4.5d, 0.0f);
+        if(trace == null || trace.getBlockPos() == null) {
+            return super.onItemRightClick(itemStack, world, player, hand);
+        }
+
+        TileEntity te = world.getTileEntity(trace.getBlockPos());
+        if(te != null && te.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, trace.sideHit)) {
+            IFluidHandler fluidHandler = world.getTileEntity(trace.getBlockPos()).getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, trace.sideHit);
+            if(fluidHandler.fill(new FluidStack(Fluidss.miniaturizationFluid, 125), false) == 125) {
+                fluidHandler.fill(new FluidStack(Fluidss.miniaturizationFluid, 125), true);
+                itemStack.stackSize--;
+                return new ActionResult(EnumActionResult.SUCCESS, itemStack);
+            }
+        }
+
+        return super.onItemRightClick(itemStack, world, player, hand);
     }
 
     @Override
