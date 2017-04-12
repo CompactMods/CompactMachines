8cfa569f0db8c11a9b82660ab50852afe0a569db
thraaawn <david.hilberath@gmx.de>
2017-04-12 19:16:56 +0200

    Removed clutter in jei ingredients list
    Hide the tunnel blocks, creative walls and mini fluid blocks
    in JEI.

--- src/main/java/org/dave/cm2/jei/CM2JEIPlugin.java
+++ src/main/java/org/dave/cm2/jei/CM2JEIPlugin.java
@@ -1,10 +1,12 @@
 package org.dave.cm2.jei;
 
 import mezz.jei.api.*;
+import mezz.jei.api.ingredients.IIngredientBlacklist;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.ForgeModContainer;
 import net.minecraftforge.fluids.UniversalBucket;
+import org.dave.cm2.init.Blockss;
 import org.dave.cm2.init.Fluidss;
 import org.dave.cm2.init.Itemss;
 import org.dave.cm2.miniaturization.MiniaturizationRecipes;
@@ -32,6 +34,11 @@ public class CM2JEIPlugin implements IModPlugin {
         registry.addRecipeCategoryCraftingItem(UniversalBucket.getFilledBucket(ForgeModContainer.getInstance().universalBucket, Fluidss.miniaturizationFluid), MultiblockRecipeCategory.UID);
         registry.addRecipes(MiniaturizationRecipes.getRecipes());
         registry.addDescription(new ItemStack(Itemss.miniFluidDrop), "cm2.jei.minifluiddrop.description");
+
+        IIngredientBlacklist blacklist = registry.getJeiHelpers().getIngredientBlacklist();
+        blacklist.addIngredientToBlacklist(new ItemStack(Blockss.tunnel));
+        blacklist.addIngredientToBlacklist(new ItemStack(Blockss.miniaturizationFluidBlock));
+        blacklist.addIngredientToBlacklist(new ItemStack(Blockss.wall));
     }
 
     @Override
