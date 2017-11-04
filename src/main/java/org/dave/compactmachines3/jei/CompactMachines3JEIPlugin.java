2dc3fc0b547e52d4b7a5e36d54a2c22087f36220
thraaawn <david.hilberath@gmx.de>
2017-11-04 15:17:37 +0100

    Replace superfluous MultiblockRecipeWrapperFactory with lambda expression

--- src/main/java/org/dave/compactmachines3/jei/CompactMachines3JEIPlugin.java
+++ src/main/java/org/dave/compactmachines3/jei/CompactMachines3JEIPlugin.java
@@ -1,6 +1,9 @@
 package org.dave.compactmachines3.jei;
 
-import mezz.jei.api.*;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
+import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ingredients.IIngredientBlacklist;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
@@ -27,7 +30,8 @@ public class CompactMachines3JEIPlugin implements IModPlugin {
     @Override
     public void register(IModRegistry registry) {
         registry.addRecipeCatalyst(new ItemStack(Blockss.fieldProjector), MultiblockRecipeCategory.UID);
-        registry.handleRecipes(MultiblockRecipe.class, new MultiblockRecipeWrapperFactory(), MultiblockRecipeCategory.UID);
+
+        registry.handleRecipes(MultiblockRecipe.class, recipe -> new MultiblockRecipeWrapper(recipe), MultiblockRecipeCategory.UID);
         registry.addRecipes(MultiblockRecipes.getRecipes(), MultiblockRecipeCategory.UID);
 
         IIngredientBlacklist blacklist = registry.getJeiHelpers().getIngredientBlacklist();
