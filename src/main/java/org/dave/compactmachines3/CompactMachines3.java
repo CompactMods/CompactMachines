f3653d8c5d392f6094dadc4b7cfcfd7ce04793be
thraaawn <david.hilberath@gmx.de>
2018-05-03 18:36:03 +0200

    Bumped to version 3.0.9

--- src/main/java/org/dave/compactmachines3/CompactMachines3.java
+++ src/main/java/org/dave/compactmachines3/CompactMachines3.java
@@ -33,7 +33,7 @@ import org.dave.compactmachines3.world.tools.DimensionTools;
 public class CompactMachines3
 {
     public static final String MODID = "compactmachines3";
-    public static final String VERSION = "3.0.8";
+    public static final String VERSION = "3.0.9";
 
     @Mod.Instance(CompactMachines3.MODID)
     public static CompactMachines3 instance;
