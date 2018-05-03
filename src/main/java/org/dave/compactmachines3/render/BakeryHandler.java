bf0034286ad0cd1c7521c4268cacd57b114d6992
thraaawn <david.hilberath@gmx.de>
2018-05-03 20:49:35 +0200

    Hotfix for #324, do not bake on the server

--- src/main/java/org/dave/compactmachines3/render/BakeryHandler.java
+++ src/main/java/org/dave/compactmachines3/render/BakeryHandler.java
@@ -2,11 +2,13 @@ package org.dave.compactmachines3.render;
 
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.dave.compactmachines3.utility.Logz;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BakeryHandler {
 
     @SubscribeEvent
+    @SideOnly(Side.CLIENT)
     public static void modelBake(ModelBakeEvent event) {
         TESRFieldProjector.prebakeModel();
     }
