0e9e3185f2f188bc5e9c9c77566a12078cb2fc3d
thraaawn <david.hilberath@gmx.de>
2019-02-20 16:44:22 +0100

    Fix preview crash caused by large NBT data
    This should fix #359

--- src/main/java/org/dave/compactmachines3/network/MessageMachineChunk.java
+++ src/main/java/org/dave/compactmachines3/network/MessageMachineChunk.java
@@ -1,6 +1,7 @@
 package org.dave.compactmachines3.network;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import org.dave.compactmachines3.CompactMachines3;
 import org.dave.compactmachines3.utility.ChunkUtils;
+import org.dave.compactmachines3.utility.Logz;
 import org.dave.compactmachines3.world.tools.DimensionTools;
 
 public class MessageMachineChunk implements IMessage, IMessageHandler<MessageMachineChunk, IMessage> {
@@ -34,7 +36,29 @@ public class MessageMachineChunk implements IMessage, IMessageHandler<MessageMac
     @Override
     public void toBytes(ByteBuf buf) {
         buf.writeInt(coords);
-        ByteBufUtils.writeTag(buf, data);
+
+        ByteBuf tmpBuf = Unpooled.buffer();
+        ByteBufUtils.writeTag(tmpBuf, data);
+
+        if(tmpBuf.writerIndex() >= 2097152 - 8) {
+            Logz.debug("Chunk data to big to send. Stripping TileEntity nbt data!");
+            data.removeTag("TileEntities");
+
+            ByteBuf tmpBufNoTiles = Unpooled.buffer();
+            ByteBufUtils.writeTag(tmpBufNoTiles, data);
+
+            if(tmpBufNoTiles.writerIndex() >= 2097152 - 8) {
+                Logz.warn("Chunk data to big even without nbt data. Sending no chunk data!");
+                ByteBufUtils.writeTag(buf, new NBTTagCompound());
+            } else {
+                buf.writeBytes(tmpBufNoTiles.readerIndex(0));
+            }
+            tmpBufNoTiles.release();
+        } else {
+            buf.writeBytes(tmpBuf.readerIndex(0));
+        }
+
+        tmpBuf.release();
     }
 
     @Override
