acdf98bc92ec8202995a26dad07838b60b3d6d00
Ted Senft <code@robotgryphon.com>
2021-03-04 18:20:38 -0500

    Fix breakable tunnels, update Tunnel Wall en_us lang

--- src/main/java/com/robotgryphon/compactmachines/block/walls/SolidWallBlock.java
+++ src/main/java/com/robotgryphon/compactmachines/block/walls/SolidWallBlock.java
@@ -1,73 +1,21 @@
 package com.robotgryphon.compactmachines.block.walls;
 
-import com.robotgryphon.compactmachines.core.Registration;
-import net.minecraft.block.Block;
+import com.robotgryphon.compactmachines.config.ServerConfig;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.fluid.FluidState;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
-import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
-public class SolidWallBlock extends Block {
+public class SolidWallBlock extends ProtectedWallBlock {
     public SolidWallBlock(Properties props) {
         super(props);
     }
 
     @Override
-    public void onBlockExploded(BlockState state, World world, BlockPos pos, Explosion explosion) {
-        // NO-Op, do not destroy on explosions
-    }
-
-    @Override
-    public boolean canHarvestBlock(BlockState state, IBlockReader world, BlockPos pos, PlayerEntity player) {
-        return false;
-    }
-
-    @Override
-    public boolean canEntityDestroy(BlockState state, IBlockReader world, BlockPos pos, Entity entity) {
-        return false;
-    }
-
-    @Override
-    public boolean canBeReplacedByLeaves(BlockState state, IWorldReader world, BlockPos pos) {
-        return false;
-    }
-
-    @Override
-    public boolean canDropFromExplosion(BlockState state, IBlockReader world, BlockPos pos, Explosion explosion) {
-        return false;
-    }
-
-    @Override
     public boolean canCreatureSpawn(BlockState state, IBlockReader world, BlockPos pos, EntitySpawnPlacementRegistry.PlacementType type, @Nullable EntityType<?> entityType) {
-        return pos.getY() == 40;
-    }
-
-    @Override
-    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        ItemStack held = player.getHeldItemMainhand();
-
-        if (held.isEmpty())
-            return ActionResultType.PASS;
-
-        if (player.isSneaking())
-            return ActionResultType.PASS;
-
-        // TODO: Tunnels and Personal Shrinking Device
-
-        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
+        return pos.getY() == ServerConfig.MACHINE_FLOOR_Y.get();
     }
 }
