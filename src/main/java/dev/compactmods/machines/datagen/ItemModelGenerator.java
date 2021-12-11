4479b85e7f545f24edc67c7661a39f181ba3eab9
Ted Senft <code@robotgryphon.com>
2021-12-10 23:42:09 -0500

    Datagen achievements, textures, models

---
+++ src/main/java/dev/compactmods/machines/datagen/ItemModelGenerator.java
@@ -0,0 +1,33 @@
+package dev.compactmods.machines.datagen;
+
+import dev.compactmods.machines.CompactMachines;
+import dev.compactmods.machines.reference.EnumMachineSize;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ItemModelGenerator extends ItemModelProvider {
+
+    public ItemModelGenerator(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, CompactMachines.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        for (var size : EnumMachineSize.values())
+            machine(size.getSerializedName());
+
+        withExistingParent("solid_wall", modLoc("block/wall"));
+        withExistingParent("wall", modLoc("block/wall"));
+
+        withExistingParent("personal_shrinking_device", mcLoc("item/generated"))
+                .texture("layer0", modLoc("item/personal_shrinking_device"));
+
+        withExistingParent("tunnel", mcLoc("item/generated"))
+                .texture("layer0", modLoc("item/tunnel"));
+    }
+
+    private void machine(String size) {
+        withExistingParent("machine_" + size, modLoc("block/machine/machine_" + size));
+    }
+}
--- src/main/java/dev/compactmods/machines/datagen/StateGenerator.java
+++ src/main/java/dev/compactmods/machines/datagen/StateGenerator.java
@@ -1,9 +1,12 @@
 package dev.compactmods.machines.datagen;
 
 import dev.compactmods.machines.CompactMachines;
+import dev.compactmods.machines.block.BlockCompactMachine;
+import dev.compactmods.machines.core.Registration;
 import dev.compactmods.machines.reference.EnumMachineSize;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class StateGenerator extends BlockStateProvider {
@@ -14,14 +17,20 @@ public class StateGenerator extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
         // Wall block model
-        models().cubeAll("block/wall", modLoc("block/wall"));
+        var wall = models().cubeAll("block/wall", modLoc("block/wall"));
+        simpleBlock(Registration.BLOCK_SOLID_WALL.get(), wall);
+        simpleBlock(Registration.BLOCK_BREAKABLE_WALL.get(), wall);
 
         // Machine models
         for(EnumMachineSize size : EnumMachineSize.values()) {
             String sizeName = size.getName();
 
-            models()
+            var mod = models()
                     .cubeAll("block/machine/machine_" + sizeName, modLoc("block/machine/machine_" + sizeName));
+
+            simpleBlock(BlockCompactMachine.getBySize(size), ConfiguredModel.builder()
+                    .modelFile(mod)
+                    .build());
         }
     }
 }
