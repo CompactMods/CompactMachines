919f391e0b224646b28ad5a80901a16c16d6f726
Ted Senft <code@robotgryphon.com>
2022-06-01 00:26:20 -0400

    Ensure that only sneaking creative players can break solid walls and tunnels

--- src/main/java/dev/compactmods/machines/wall/ProtectedWallBlock.java
+++ src/main/java/dev/compactmods/machines/wall/ProtectedWallBlock.java
@@ -3,14 +3,29 @@ package dev.compactmods.machines.wall;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.FluidState;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class ProtectedWallBlock extends Block {
     protected ProtectedWallBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        if(!canPlayerBreak(level, player, pos))
+            return false;
+
+        level.gameEvent(player, GameEvent.BLOCK_DESTROY, pos);
+        return level.setBlock(pos, fluid.createLegacyBlock(), level.isClientSide ? 11 : 3);
     }
 
     @Override
@@ -26,5 +41,12 @@ public abstract class ProtectedWallBlock extends Block {
     @Override
     public boolean canDropFromExplosion(BlockState state, BlockGetter world, BlockPos pos, Explosion explosion) {
         return false;
+    }
+
+    public boolean canPlayerBreak(Level level, Player player, BlockPos pos) {
+        if(!player.isCreative()) return false;
+        if(!player.isShiftKeyDown()) return false;
+
+        return true;
     }
 }
