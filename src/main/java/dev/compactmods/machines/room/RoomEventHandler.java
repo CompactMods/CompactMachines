bcc59703b34b630c7be73e37d14ceaa23cf83035
Ted Senft <code@robotgryphon.com>
2022-06-01 00:26:57 -0400

    Disallow more ways for mobs to escape rooms; closes #491

--- src/main/java/dev/compactmods/machines/room/RoomEventHandler.java
+++ src/main/java/dev/compactmods/machines/room/RoomEventHandler.java
@@ -2,7 +2,6 @@ package dev.compactmods.machines.room;
 
 import dev.compactmods.machines.CompactMachines;
 import dev.compactmods.machines.api.core.Messages;
-import dev.compactmods.machines.core.MissingDimensionException;
 import dev.compactmods.machines.core.Registration;
 import dev.compactmods.machines.room.data.CompactRoomData;
 import dev.compactmods.machines.i18n.TranslationUtil;
@@ -10,35 +9,52 @@ import dev.compactmods.machines.room.exceptions.NonexistentRoomException;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.ChatFormatting;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.EntityTeleportEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = CompactMachines.MOD_ID)
-public class TeleportationEventHandler {
+public class RoomEventHandler {
 
     @SubscribeEvent
-    public static void onEnderTeleport(final EntityTeleportEvent.EnderEntity evt) {
-        Vec3 target = new Vec3(
-                evt.getTargetX(),
-                evt.getTargetY(),
-                evt.getTargetZ()
-        );
+    public static void entityJoined(final EntityJoinWorldEvent evt) {
+        Entity ent = evt.getEntity();
+
+        // Early exit if spawning in non-CM dimensions
+        if ((ent instanceof Player) || !ent.level.dimension().equals(Registration.COMPACT_DIMENSION)) return;
+
+        // no-op clients, we only care about blocking server spawns
+        if(ent.level.isClientSide) return;
+
+        if (!positionInsideRoom(ent, ent.position())) {
+            evt.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onCheckSpawn(final LivingSpawnEvent.CheckSpawn evt) {
+        Vec3 target = new Vec3(evt.getX(), evt.getY(), evt.getZ());
 
         Entity ent = evt.getEntity();
-        doEntityTeleportHandle(evt, target, ent);
+
+        // Early exit if spawning in non-CM dimensions
+        if (!ent.level.dimension().equals(Registration.COMPACT_DIMENSION)) return;
+
+        if (!positionInsideRoom(ent, target)) evt.setResult(Event.Result.DENY);
     }
 
     @SubscribeEvent
     public static void onEntityTeleport(final EntityTeleportEvent evt) {
         // Allow teleport commands, we don't want to trap people anywhere
-        if (evt instanceof EntityTeleportEvent.TeleportCommand)
-            return;
+        if (evt instanceof EntityTeleportEvent.TeleportCommand) return;
 
         Entity ent = evt.getEntity();
         doEntityTeleportHandle(evt, evt.getTarget(), ent);
@@ -51,21 +67,19 @@ public class TeleportationEventHandler {
      *
      * @param entity Entity trying to teleport.
      * @param target Teleportation target location.
-     * @return True if teleportation should be cancelled; false otherwise.
+     * @return True if position is inside a room; false otherwise.
      */
-    private static boolean cancelOutOfBoxTeleport(Entity entity, Vec3 target) {
+    private static boolean positionInsideRoom(Entity entity, Vec3 target) {
         final var level = entity.level;
-        if (!level.dimension().equals(Registration.COMPACT_DIMENSION))
-            return false;
+        if (!level.dimension().equals(Registration.COMPACT_DIMENSION)) return false;
 
-        if(level instanceof ServerLevel compactDim) {
+        if (level instanceof ServerLevel compactDim) {
             ChunkPos machineChunk = new ChunkPos(entity.chunkPosition().x, entity.chunkPosition().z);
 
             try {
                 final CompactRoomData intern = CompactRoomData.get(compactDim);
-                return !intern.getBounds(machineChunk).contains(target);
+                return intern.getBounds(machineChunk).contains(target);
             } catch (NonexistentRoomException e) {
-                CompactMachines.LOGGER.error(e);
                 return false;
             }
         }
@@ -74,18 +88,11 @@ public class TeleportationEventHandler {
     }
 
     private static void doEntityTeleportHandle(EntityEvent evt, Vec3 target, Entity ent) {
-        if (ent.level.dimension() == Registration.COMPACT_DIMENSION) {
-            if (cancelOutOfBoxTeleport(ent, target) && evt.isCancelable()) {
-                if (ent instanceof ServerPlayer) {
-                    ((ServerPlayer) ent).displayClientMessage(
-                            TranslationUtil.message(Messages.TELEPORT_OUT_OF_BOUNDS, ent.getName())
-                                    .withStyle(ChatFormatting.RED)
-                                    .withStyle(ChatFormatting.ITALIC),
-                            true
-                    );
-                }
-                evt.setCanceled(true);
+        if (!positionInsideRoom(ent, target) && evt.isCancelable()) {
+            if (ent instanceof ServerPlayer) {
+                ((ServerPlayer) ent).displayClientMessage(TranslationUtil.message(Messages.TELEPORT_OUT_OF_BOUNDS, ent.getName()).withStyle(ChatFormatting.RED).withStyle(ChatFormatting.ITALIC), true);
             }
+            evt.setCanceled(true);
         }
     }
 }
