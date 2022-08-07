2a9d42bdf8a5c273cee52e843dc5f29f9d42fc75
Ted Senft <code@robotgryphon.com>
2022-08-07 00:09:19 -0400

    Move tunnel wall render type to datagen/model files

--- src/main/java/dev/compactmods/machines/client/ClientEventHandler.java
+++ src/main/java/dev/compactmods/machines/client/ClientEventHandler.java
@@ -1,14 +1,12 @@
 package dev.compactmods.machines.client;
 
 import dev.compactmods.machines.api.core.Constants;
-import dev.compactmods.machines.tunnel.Tunnels;
 import dev.compactmods.machines.core.UIRegistration;
 import dev.compactmods.machines.room.client.MachineRoomScreen;
+import dev.compactmods.machines.tunnel.Tunnels;
 import dev.compactmods.machines.tunnel.client.TunnelColors;
 import dev.compactmods.machines.tunnel.client.TunnelItemColor;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -30,9 +28,6 @@ public class ClientEventHandler {
 
     @SubscribeEvent
     public static void onClientSetup(final FMLClientSetupEvent client) {
-        RenderType cutout = RenderType.cutoutMipped();
-        ItemBlockRenderTypes.setRenderLayer(Tunnels.BLOCK_TUNNEL_WALL.get(), cutout);
-
         MenuScreens.register(UIRegistration.MACHINE_MENU.get(), MachineRoomScreen::new);
     }
 }
