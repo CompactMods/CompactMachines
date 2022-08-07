fbcc0b3cb2af1bc8a599f6ebf4be68f72c99ff49
Ted Senft <code@robotgryphon.com>
2022-08-07 00:09:53 -0400

    Fix machine data file export

--- src/main/java/dev/compactmods/machines/command/data/CMMachineDataExportCommand.java
+++ src/main/java/dev/compactmods/machines/command/data/CMMachineDataExportCommand.java
@@ -2,15 +2,21 @@ package dev.compactmods.machines.command.data;
 
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
-import dev.compactmods.machines.api.room.MachineRoomConnections;
+import dev.compactmods.machines.CompactMachines;
+import dev.compactmods.machines.api.core.CMCommands;
 import dev.compactmods.machines.api.core.Constants;
+import dev.compactmods.machines.api.location.IDimensionalBlockPosition;
+import dev.compactmods.machines.i18n.TranslationUtil;
+import dev.compactmods.machines.machine.graph.DimensionMachineGraph;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.util.CsvOutput;
+import net.minecraft.world.level.ChunkPos;
 
 import javax.annotation.Nonnull;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 // TODO
 public class CMMachineDataExportCommand {
@@ -25,35 +31,32 @@ public class CMMachineDataExportCommand {
         var src = ctx.getSource();
         var serv = src.getServer();
 
-//        final CompactMachineGraph machines;
-//        final MachineToRoomConnections connections;
-//        try {
-//            machines = CompactMachineGraph.forDimension(serv);
-//            connections = MachineToRoomConnections.get(serv);
-//        } catch (MissingDimensionException e) {
-//            CompactMachines.LOGGER.fatal(e);
-//            return -1;
-//        }
-//
-//        var outdir = src.getServer().getFile(CompactMachines.MOD_ID);
-//        var out = outdir.toPath()
-//                .resolve("machines.csv")
-//                .toAbsolutePath();
-//
-//        try {
-//            Files.createDirectories(outdir.toPath());
-//
-//            var writer = Files.newBufferedWriter(out);
-//            CsvOutput builder = makeCsv(writer);
-//
-//            machines.getMachines().forEach(node -> writeMachine(connections, node, builder));
-//
-//            writer.close();
-//        } catch (IOException e) {
-//            CompactMachines.LOGGER.error(e);
-//            src.sendFailure(TranslationUtil.command(CMCommands.FAILED_CMD_FILE_ERROR));
-//            return -1;
-//        }
+        var outdir = src.getServer().getFile(Constants.MOD_ID);
+        var out = outdir.toPath()
+                .resolve("machines.csv")
+                .toAbsolutePath();
+
+        try {
+            Files.createDirectories(outdir.toPath());
+
+            var writer = Files.newBufferedWriter(out);
+            CsvOutput builder = makeCsv(writer);
+
+            for (final var dim : serv.getAllLevels()) {
+                final var graph = DimensionMachineGraph.forDimension(dim);
+                graph.getMachines().forEach(machNode -> {
+                    final var m = machNode.dimpos();
+                    final var r = graph.getConnectedRoom(m.getBlockPosition());
+                    r.ifPresent(room -> writeMachine(m, room, builder));
+                });
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            CompactMachines.LOGGER.error(e);
+            src.sendFailure(TranslationUtil.command(CMCommands.FAILED_CMD_FILE_ERROR));
+            return -1;
+        }
 
         return 0;
     }
@@ -61,7 +64,6 @@ public class CMMachineDataExportCommand {
     @Nonnull
     private static CsvOutput makeCsv(BufferedWriter writer) throws IOException {
         return CsvOutput.builder()
-                .addColumn("id")
                 .addColumn("dim")
                 .addColumn("machine_x")
                 .addColumn("machine_y")
@@ -71,24 +73,20 @@ public class CMMachineDataExportCommand {
                 .build(writer);
     }
 
-    private static void writeMachine(MachineRoomConnections connections, CsvOutput builder) {
-//        try {
-//            int id = mach.getMachineId();
-//            var loc = mach.getLocation();
-//            var placedAt = loc.getBlockPosition();
-//
-//            var room = connections.getConnectedRoom(id).orElse(new ChunkPos(-1, -1));
-//            builder.writeRow(
-//                    id,
-//                    loc.getDimension().location().toString(),
-//                    placedAt.getX(),
-//                    placedAt.getY(),
-//                    placedAt.getZ(),
-//                    room.x,
-//                    room.z
-//            );
-//        } catch (IOException e) {
-//            CompactMachines.LOGGER.error(e);
-//        }
+    private static void writeMachine(IDimensionalBlockPosition machine, ChunkPos room, CsvOutput builder) {
+        try {
+            var placedAt = machine.getBlockPosition();
+
+            builder.writeRow(
+                    machine.dimensionKey().location().toString(),
+                    placedAt.getX(),
+                    placedAt.getY(),
+                    placedAt.getZ(),
+                    room.x,
+                    room.z
+            );
+        } catch (IOException e) {
+            CompactMachines.LOGGER.error(e);
+        }
     }
 }
