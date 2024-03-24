17bad698741c4edf876a68ab6852e29f532b10f6
Ted Senft <code@robotgryphon.com>
2024-03-24 00:17:26 -0400

    Refactor tag generator, add support for Shrink as a way into machines

--- neoforge-datagen/src/main/java/dev/compactmods/machines/datagen/tags/ItemTagGenerator.java
+++ neoforge-datagen/src/main/java/dev/compactmods/machines/datagen/tags/ItemTagGenerator.java
@@ -1,15 +1,21 @@
 package dev.compactmods.machines.datagen.tags;
 
 import dev.compactmods.machines.api.machine.MachineConstants;
+import dev.compactmods.machines.api.room.upgrade.RoomUpgrade;
 import dev.compactmods.machines.api.shrinking.PSDTags;
+import dev.compactmods.machines.api.util.KeyHelper;
 import dev.compactmods.machines.neoforge.Registries;
 import dev.compactmods.machines.neoforge.machine.Machines;
+import dev.compactmods.machines.neoforge.shrinking.PersonalShrinkingDevice;
 import dev.compactmods.machines.neoforge.shrinking.Shrinking;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -20,7 +26,31 @@ public class ItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        // var upgradeTag = tag(CMTags.ROOM_UPGRADE_ITEM);
+        final var psd = Shrinking.PERSONAL_SHRINKING_DEVICE.get();
+
+        machines();
+        shrinkingDevices(psd);
+        curiosTags(psd);
+        upgrades();
+    }
+
+    private void upgrades() {
+        var upgradeTag = tag(RoomUpgrade.ITEM_TAG);
+        // upgradeTag.add(Items.IRON_AXE);
+    }
+
+    private void shrinkingDevices(PersonalShrinkingDevice psd) {
+        final var cmShrinkTag = tag(PSDTags.ITEM);
+        cmShrinkTag.add(psd);
+        cmShrinkTag.add(ResourceKey.create(net.minecraft.core.registries.Registries.ITEM, new ResourceLocation("shrink", "shrinking_device")));
+    }
+
+    private void curiosTags(PersonalShrinkingDevice psd) {
+        final var curiosPsdTag = tag(TagKey.create(Registries.ITEMS.getRegistryKey(), new ResourceLocation("curios", "psd")));
+        curiosPsdTag.add(psd);
+    }
+
+    private void machines() {
         var machinesTag = tag(MachineConstants.MACHINE_ITEM);
 
         var boundMachineItem = Machines.BOUND_MACHINE_BLOCK_ITEM.get();
@@ -28,11 +58,5 @@ public class ItemTagGenerator extends ItemTagsProvider {
 
         machinesTag.add(boundMachineItem);
         machinesTag.add(unboundMachineItem);
-
-        final var psd = Shrinking.PERSONAL_SHRINKING_DEVICE.get();
-        final var curiosPsdTag = tag(TagKey.create(Registries.ITEMS.getRegistryKey(), new ResourceLocation("curios", "psd")));
-        final var cmShrinkTag = tag(PSDTags.ITEM);
-        curiosPsdTag.add(psd);
-        cmShrinkTag.add(psd);
     }
 }
